390782f1ba85a78449987c335803522517a03a17
Josh Bonfield <josh@grizzlyware.io>
2019-06-24 18:29:50 +0100

    Move the directory

--- scripts/buildWhmcsModule.php
+++ scripts/buildWhmcsModule.php
@@ -3,6 +3,6 @@
 require __DIR__ . "/../vendor/autoload.php";
 
 $rootPath = realpath(__DIR__ . "/../");
-$whmcsTemplatePath = realpath(__DIR__ . "/../dist/whmcs/modules/addons/salmon_template");
+$whmcsTemplatePath = realpath(__DIR__ . "/../dist/whmcs/modules/addons/salmon");
 
 exec("cd {$whmcsTemplatePath}; php {$rootPath}/composer.phar update");
