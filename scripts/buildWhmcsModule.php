0ec218a3ec1de7970f2df7783394626cc51590e6
Josh Bonfield <josh@grizzlyware.io>
2019-06-24 18:28:37 +0100

    Call composer to build the WHMCS module

--- scripts/buildWhmcsModule.php
+++ scripts/buildWhmcsModule.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+$rootPath = realpath(__DIR__ . "/../");
 $whmcsTemplatePath = realpath(__DIR__ . "/../dist/whmcs/modules/addons/salmon_template");
 
-exec("cd {$whmcsTemplatePath}; composer update");
+exec("cd {$whmcsTemplatePath}; php {$rootPath}/composer.phar update");
